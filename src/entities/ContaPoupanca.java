6f1e5b57f407dfd1119d35e40f803d204f215bc8
ludmilact <ludmilatimoteo@Outlook.com>
2022-07-25 22:29:51 -0300

    Implementação do atributo 'diaAniversarioPoupanca' e do método 'correcaoAniversario', lógica principal desta classe.

--- src/entities/ContaPoupanca.java
+++ src/entities/ContaPoupanca.java
@@ -2,5 +2,22 @@ package entities;
 
 import conta.Conta;
 
+import java.time.LocalDateTime;
+
 public class ContaPoupanca extends Conta {
+
+    LocalDateTime diaAniversarioPoupanca;
+
+    public ContaPoupanca(int numero, String cpf, LocalDateTime diaAniversarioPoupanca) {
+        super(numero, cpf);
+        this.diaAniversarioPoupanca = diaAniversarioPoupanca;
+    }
+    public float correcaoAniversario() {
+        if (diaAniversarioPoupanca.equals(LocalDateTime.now())) {
+            this.setSaldo((this.getSaldo() * 0.05f) + this.getSaldo());
+        } else {
+            this.setSaldo((this.getSaldo() * 0.04f) + this.getSaldo());
+        }
+        return getSaldo();
+    }
 }
