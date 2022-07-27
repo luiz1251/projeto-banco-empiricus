485c9c158ceb4332ee9b41e7653c67c4f5f38a7a
Luiz <luizfernando1251@gmail.com>
2022-07-26 21:59:17 -0400

    Base da classe com algumas funcionalidades.

--- src/entities/ContaEmpresarial.java
+++ src/entities/ContaEmpresarial.java
@@ -3,4 +3,26 @@ package entities;
 import conta.Conta;
 
 public class ContaEmpresarial extends Conta {
+	private float emprestimoEmpresa;
+
+	public ContaEmpresarial(int numero, String cpf) {
+		super(numero, cpf);
+		this.setEmprestimoEmpresa(10000);
+	}
+	
+	public void pedirEmprestimo(float valorDoEmprestimo) {
+		if(valorDoEmprestimo <= getEmprestimoEmpresa()) {
+			credito(valorDoEmprestimo);
+			setEmprestimoEmpresa(getEmprestimoEmpresa() - valorDoEmprestimo);
+		}
+	}
+
+	public float getEmprestimoEmpresa() {
+		return emprestimoEmpresa;
+	}
+
+	public void setEmprestimoEmpresa(float emprestimoEmpresa) {
+		this.emprestimoEmpresa = emprestimoEmpresa;
+	}
+	
 }
